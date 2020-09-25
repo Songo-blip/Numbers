369e1fde167de8cce388c5740464b8085c8d9f97
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-09-25 14:22:32 +0200

    Add project files.

---
+++ Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int counter = 500000; counter >= 0; counter --)
+                   {
+                              string[] numberTexts = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+                string[] numberTexts2 = { "eleven", "twelve", "thirteen", "fourteen", "firfteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] numberTexts3 = { "twenty", "thirthy", "forthy", "fifty", "sixty", "seventy" };
+            }
+            Console.WriteLine("");
+            }
+        }
+    }
+}
